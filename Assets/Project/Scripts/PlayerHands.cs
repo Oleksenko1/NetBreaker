ac80f75eb2d862b4963476bddf5bf8b10995b53b
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-10-15 23:10:32 +0300

    Add animation to the hands

--- Assets/Project/Scripts/PlayerHands.cs
+++ Assets/Project/Scripts/PlayerHands.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,10 @@ public class PlayerHands : MonoBehaviour
     [SerializeField] private Transform rightHand;
     [Space(10)]
     [SerializeField] private RectTransform canvasRT;
+    private Vector3 leftHandStartPos;
+    private Vector3 rightHandStartPos;
+    Vector3[] canvasAngles = new Vector3[4];
+    private bool lastHandLeft = true; // If last moved hand in animation was left
     void Awake()
     {
         EventBus.Subscribe<ClickPressed_event>(OnClickPressed);
@@ -18,19 +23,42 @@ public class PlayerHands : MonoBehaviour
     }
     private void OnClickPressed(ClickPressed_event e)
     {
+        PlayClickAnim();
+    }
+    private void ResetHandsPos()
+    {
+        Transform handTR = lastHandLeft ? rightHand : leftHand;
+        Vector3 handStartPos = lastHandLeft ? rightHandStartPos : leftHandStartPos;
 
+        DOTween.Kill(handTR);
+        handTR.position = handStartPos;
+    }
+    private void PlayClickAnim()
+    {
+        ResetHandsPos();
+
+        Transform handTR = lastHandLeft ? rightHand : leftHand;
+        Vector3 handStartPos = lastHandLeft ? rightHandStartPos : leftHandStartPos;
+
+        handTR.DOMoveY(handStartPos.y + downMoveOffset, 0.4f).SetEase(Ease.OutBack);
+
+        lastHandLeft = !lastHandLeft;
     }
     private void InitializeTransforms()
     {
         Vector3[] canvasAngles = new Vector3[4];
         canvasRT.GetWorldCorners(canvasAngles);
 
-        var leftHandPos = leftHand.position;
-        leftHandPos.x = canvasAngles[0].x;
-        leftHand.position = leftHandPos;
+        rightHandStartPos = rightHand.position;
+        rightHandStartPos.x = canvasAngles[2].x;
+        rightHandStartPos.y -= downMoveOffset;
 
-        var rightHandPos = rightHand.position;
-        rightHandPos.x = canvasAngles[2].x;
-        rightHand.position = rightHandPos;
+        leftHandStartPos = leftHand.position;
+        leftHandStartPos.x = canvasAngles[0].x;
+        leftHandStartPos.y -= downMoveOffset;
+
+        // Invoked 2 times so both hands would reset
+        PlayClickAnim();
+        PlayClickAnim();
     }
 }
