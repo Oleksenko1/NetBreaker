747d9c1905174afdb0e2e9f27b343d491790fb15
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-10-15 21:31:56 +0300

    Add repositioning of the hands depending on the screen size

--- Assets/Project/Scripts/ClickingZone.cs
+++ Assets/Project/Scripts/ClickingZone.cs
@@ -3,12 +3,13 @@ using UnityEngine.EventSystems;
 
 public class ClickingZone : MonoBehaviour, IPointerDownHandler
 {
+    private ClickPressed_event clickPressed_Event;
     void Start()
     {
-
+        clickPressed_Event = new ClickPressed_event();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Click");
+        EventBus.Publish(clickPressed_Event);
     }
 }
--- Assets/Project/Scripts/PlayerHands.cs
+++ Assets/Project/Scripts/PlayerHands.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHands : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Values")]
+    [SerializeField] private float downMoveOffset;
+    [Header("Components")]
+    [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHand;
+    [Space(10)]
+    [SerializeField] private RectTransform canvasRT;
+    void Awake()
     {
-        
-    }
+        EventBus.Subscribe<ClickPressed_event>(OnClickPressed);
 
-    // Update is called once per frame
-    void Update()
+        InitializeTransforms();
+    }
+    private void OnClickPressed(ClickPressed_event e)
     {
-        
+
+    }
+    private void InitializeTransforms()
+    {
+        Vector3[] canvasAngles = new Vector3[4];
+        canvasRT.GetWorldCorners(canvasAngles);
+
+        var leftHandPos = leftHand.position;
+        leftHandPos.x = canvasAngles[0].x;
+        leftHand.position = leftHandPos;
+
+        var rightHandPos = rightHand.position;
+        rightHandPos.x = canvasAngles[2].x;
+        rightHand.position = rightHandPos;
     }
 }
