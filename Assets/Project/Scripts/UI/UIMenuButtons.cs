4a2b5c11774eff5d0601bae655323a4b64b2346f
Bogdan Oleksenko <bogdanoleksenko2004@gmail.com>
2025-10-18 23:18:15 +0300

    Add nimations of opening/closing menu buttons

---
+++ Assets/Project/Scripts/UI/UIMenuButtons.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenuButtons : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Button toggleMenuBtn;
+    [SerializeField] private Button openSettingsBtn;
+    [SerializeField] private Button openUpgradesBtn;
+    [Space(10)]
+    [SerializeField] private VerticalLayoutGroup verticalLayout;
+    private RectTransform toggleMenuBtnRT;
+    private bool isMenuOpen;
+    private const float openSpacingOfLayout = 40f;
+    private const float closedSpacingOfLayout = -160;
+
+    private Tweener spacingTween;
+    private Tweener rotationTween;
+
+    private void Awake()
+    {
+        toggleMenuBtnRT = toggleMenuBtn.GetComponent<RectTransform>();
+        toggleMenuBtn.onClick.AddListener(OnToggleMenuPressed);
+
+        verticalLayout.spacing = closedSpacingOfLayout;
+        toggleMenuBtnRT.rotation = Quaternion.identity;
+    }
+
+    private void OnToggleMenuPressed()
+    {
+        float targetSpacing = isMenuOpen ? closedSpacingOfLayout : openSpacingOfLayout;
+        Vector3 rotation = isMenuOpen ? Vector3.zero : Vector3.forward * 180;
+        float animDuration = 0.3f;
+
+        spacingTween?.Kill();
+        rotationTween?.Kill();
+
+        spacingTween = DOTween.To(
+            () => verticalLayout.spacing,
+            x => verticalLayout.spacing = x,
+            targetSpacing,
+            animDuration
+        ).SetEase(Ease.InCubic);
+
+        DOVirtual.DelayedCall(animDuration * 0.5f, () =>
+        {
+            rotationTween = toggleMenuBtnRT.DORotate(rotation, animDuration / 2)
+                .SetEase(Ease.Linear);
+        });
+
+        isMenuOpen = !isMenuOpen;
+    }
+
+    private void OnDestroy()
+    {
+        spacingTween?.Kill();
+        rotationTween?.Kill();
+    }
+}
